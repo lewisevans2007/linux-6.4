5a9bb7e739beb1639534aa694c2a4946a4feacaa
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-28 20:36:52 +0100

    lint bot: Add suppression
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,17 @@
 # Place tesing script in here to build and test linux kernel
 # Then run make test
 
-make -j 24
-qemu-system-x86_64 -kernel arch/x86/boot/bzImage -initrd ~/initramfs.cpio.gz --append "root=/dev/ram init=/init console=ttyS0" -nographic
+
+
+qemu-system-x86_64 -kernel arch/x86/boot/bzImage -initrd ~/initramfs.cpio.gz \
+  --append "root=/dev/ram init=/init console=ttyS0" -nographic \
+  -device usb-ehci,id=ehci \
+  -device usb-kbd \
+  -device virtio-rng-pci \
+  -device virtio-net-pci,netdev=usernet \
+  -netdev user,id=usernet,hostfwd=tcp::5555-:22 \
+  -device virtio-gpu-pci \
+  -device usb-tablet \
+  -device usb-mouse \
+  -device virtio-serial-pci \
+  -device virtio-net-pci \
